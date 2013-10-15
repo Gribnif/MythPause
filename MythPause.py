1dab64e0fa7c08936ab612680678174d0e184544
gribnif <drupal@dwilga-linux1.amherst.edu>
2013-10-14 20:37:10 -0400

    Document the --swap mode

--- MythPause.py
+++ MythPause.py
@@ -1,5 +1,5 @@
 #  MythPause.py
-#  Copyright 2012 Dan Wilga
+#  Copyright 2013 Dan Wilga
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
